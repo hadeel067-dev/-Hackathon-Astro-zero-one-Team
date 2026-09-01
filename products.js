10a32b9e0dff69ea0c73d444f238d3507a715979
MalakAbumunshar <abumunsharmalak@gmail.com>
2026-09-01 11:10:00 +0300

    Update projects

--- products.js
+++ products.js
@@ -39,6 +39,44 @@ const products = [
         category: "ملابس",
         Image: "./imgs/download (3).jpg",
         description: "ثوب فلسطيني بتطريز يدوي مميز"
+    }
+    {
+        id: 6,
+        name: "زيت زيتون من اللأراضي الفلسطينية",
+        price: 70,
+        category: "أكلاتنا",
+        Image: "./imgs/Zeytin ile Zeytinyağı.jfif",
+        description: "زيت زيتون برائحة الارض"
+    }{
+        id: 7,
+        name: "مخللات",
+        price: 5.5,
+        category: "أكلاتنا",
+        Image: "./imgs/تنزيل.jfif",
+        description: "مخللات شهيّة"
+    }{
+        id: 8,
+        name: "زعتر فلسطيني",
+        price: 5,
+        category: "أكلاتنا",
+        Image: "./imgs/زعتر - Kamal studiotr1.jfif",
+        description: "زعتر فلسطيني مميز"
+    }{
+        id: 9,
+        name: "ربطة عنق مطرزة",
+        price: 7,
+        category: "ملابس",
+        Image: "./imgs/Cross-stitch tie.jfif",
+        description: "ربطة عنق فلسطينية بتطريز يدوي مميز"
+    }{
+        id: 10,
+        name: "كوفيه فلسطينية",
+        price: 20,
+        category: "ملابس",
+        Image: "imgs/Black and White Hirbawi® Kufiya.jfif",
+        description: "كوفية فلسطينية تقليدية"
+    }
+
    
 
 
