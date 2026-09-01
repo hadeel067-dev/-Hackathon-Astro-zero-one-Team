3820ed6ba7bb966802a779795ea0aed8bc63693d
mayar dahnous <mayardh27@gmail.com>
2026-09-01 11:35:05 +0300

    Update index.html

--- divid.js
+++ divid.js
@@ -16,6 +16,5 @@ categoryButtons.forEach(button => {
 
 })
 
-}
-
 })
+
--- prod2.js
+++ prod2.js
@@ -7,12 +7,11 @@ function displayProducts(productsToDisplay) {
 
     productsToDisplay.forEach(product => {
 
-    
+      
         const card = document.createElement("div");
 
         card.classList.add("product-card");
 
-        
         card.innerHTML = `
             <img src="${product.Image}" alt="${product.name}">
 
@@ -34,38 +33,37 @@ function displayProducts(productsToDisplay) {
        
         const addButton = card.querySelector(".add-to-card");
 
-        
         addButton.onclick = function () {
 
-            
+           
             let cart =
                 JSON.parse(localStorage.getItem("cart")) || [];
 
-            
+           
             let existingProduct =
                 cart.find(item => item.id === product.id);
 
             if (existingProduct) {
 
-                
+               
                 existingProduct.quantity += 1;
 
             } else {
 
-             
+                
                 cart.push({
                     ...product,
                     quantity: 1
                 });
             }
 
-          
+            
             localStorage.setItem(
                 "cart",
                 JSON.stringify(cart)
             );
 
-            
+           
             updateCartCount();
         };
     });
