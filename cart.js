4458c91507f82a15f663031b338470dbbf6781f6
Mayar Dahnous <mayardh27@gmail.com>
2026-09-01 12:41:30 +0300

    Refactor displayCart function and improve cart display

--- cart.js
+++ cart.js
@@ -45,4 +45,98 @@ function displayCart() {
     total.innerText = `$${totalPrice}`;
 }
 
+displayCart();let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+const list = document.querySelector(".cart-list");
+const total = document.querySelector(".total");
+
+function displayCart() {
+
+    list.innerHTML = "";
+
+    let totalPrice = 0;
+
+    cart.forEach((product, index) => {
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            <img src="${product.Image}" width="80">
+
+            <span>${product.name}</span>
+
+            <span>$${product.price}</span>
+
+            <span>الكمية: ${product.quantity}</span>
+
+            <button class="removefromcard">
+                حذف
+            </button>
+        `;
+
+        list.appendChild(li);
+
+        totalPrice += product.price * product.quantity;
+
+        li.querySelector(".removefromcard").onclick = function () {
+
+            cart.splice(index, 1);
+
+            localStorage.setItem("cart", JSON.stringify(cart));
+
+            displayCart();
+        };
+
+    });
+
+    total.innerText = `$${totalPrice}`;
+}
+
+displayCart();let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+const list = document.querySelector(".cart-list");
+const total = document.querySelector(".total");
+
+function displayCart() {
+
+    list.innerHTML = "";
+
+    let totalPrice = 0;
+
+    cart.forEach((product, index) => {
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            <img src="${product.Image}" width="80">
+
+            <span>${product.name}</span>
+
+            <span>$${product.price}</span>
+
+            <span>الكمية: ${product.quantity}</span>
+
+            <button class="removefromcard">
+                حذف
+            </button>
+        `;
+
+        list.appendChild(li);
+
+        totalPrice += product.price * product.quantity;
+
+        li.querySelector(".removefromcard").onclick = function () {
+
+            cart.splice(index, 1);
+
+            localStorage.setItem("cart", JSON.stringify(cart));
+
+            displayCart();
+        };
+
+    });
+
+    total.innerText = `$${totalPrice}`;
+}
+
 displayCart();
